b7a7391e9fb9d8efed0333fc31a1518e04f3b81b
Antima Jain <antimajain915@gmail.com>
2020-10-09 00:06:18 +0530

    Create pairwise_swap.cpp

---
+++ LinkedList/C++/pairwise_swap.cpp
@@ -0,0 +1,78 @@
+// C++ program to pairwise swap elements 
+// in a given linked list 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+/* A linked list node */
+class Node { 
+public: 
+	int data; 
+	Node* next; 
+}; 
+
+/* Function to pairwise swap elements of a linked list */
+void pairWiseSwap(Node* head) 
+{ 
+	Node* temp = head; 
+
+	/* Traverse further only if there are at-least two nodes left */
+	while (temp != NULL && temp->next != NULL) { 
+		/* Swap data of node with its next node's data */
+		swap(temp->data, 
+			temp->next->data); 
+
+		/* Move temp by 2 for the next pair */
+		temp = temp->next->next; 
+	} 
+} 
+
+// Function to add a node at the beginning of Linked List 
+void push(Node** head_ref, int new_data) 
+{ 
+//	 allocate node 
+	Node* new_node = new Node(); 
+
+//	 put in the data 
+	new_node->data = new_data; 
+
+//	link the old list off the new node 
+	new_node->next = (*head_ref); 
+
+//	 move the head to point to the new node 
+	(*head_ref) = new_node; 
+} 
+
+// Function to print nodes in a given linked list
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data << " "; 
+		node = node->next; 
+	} 
+} 
+
+int main() 
+{ 
+	Node* start = NULL; 
+	int n,x;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>x;
+		push(&start, x);
+	}
+
+	cout << "Linked list "
+		<< "before swapping\n"; 
+	printList(start); 
+
+	pairWiseSwap(start); 
+
+	cout << "\nLinked list "
+		<< "after swapping\n"; 
+	printList(start); 
+
+	return 0; 
+} 
+
+
